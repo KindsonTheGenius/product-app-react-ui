1ea48f614885586cfbab523f4d0dac722d39688e
KindsonTheGenius <Smart24*>
2024-09-20 14:22:14 +0200

    Fixed conflicts

--- src/main/java/com/kindsonthegenius/product_app/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kindsonthegenius.product_app.repositories;
-
-import com.kindsonthegenius.product_app.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
